0a372c1f356550d25ae95973749f201b1d6d930d
Shashank Agrawal <shagrawal99@gmail.com>
2017-05-02 11:00:00 +0530

    Added integration test for UserService and test passing.

---
+++ src/integration-test/groovy/com/test/UserServiceSpec.groovy
@@ -0,0 +1,48 @@
+package com.test
+
+import grails.test.mixin.integration.Integration
+import grails.transaction.Rollback
+import spock.lang.Specification
+
+@Integration
+@Rollback
+class UserServiceSpec extends Specification implements IntegrationTestHelper {
+
+    UserService userService
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test save when a user with same email already exists"() {
+        given: "An existing user"
+        basicSetup()
+        assert user1.id
+        assert user1.email == "john@example.com"
+
+        when: "A new user is saved with the same email"
+        User newUser = userService.save([email: "john@example.com", password: "test"])
+
+        then: "The user should not be saved"
+        !newUser.id
+        newUser.hasErrors()
+        newUser.errors.hasFieldErrors("email")
+    }
+
+    void "test save"() {
+        given: "An existing user"
+        basicSetup()
+        assert user1.id
+        assert user1.email == "john@example.com"
+
+        when: "A new user is saved with a different email"
+        User newUser = userService.save([email: "donald@example.com", password: "test"])
+
+        then: "The user should be saved"
+        newUser.id
+        !newUser.hasErrors()
+        newUser.email == "donald@example.com"
+    }
+}
